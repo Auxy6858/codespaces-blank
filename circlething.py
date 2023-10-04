14469459459d3fecbd51a74d8487c328ee61954e
Auxy6858 <71662994+Auxy6858@users.noreply.github.com>
2023-10-04 21:42:47 +0100

    Update circlething.py
    I forgot a pair of brackets

--- circlething.py
+++ circlething.py
@@ -2,7 +2,7 @@ Pi = float(3.142)
 REPEAT = True
 while REPEAT == True:
   Userchoice = input("Do you want to run the program (y/n) ")
-  if Userchoice.lower == "y": 
+  if Userchoice.lower() == "y":
     UserRadius = float(input("What's the radius of the circle?\n"))
     CircleArea = float(UserRadius*UserRadius*Pi)
     print("The area is ", str(CircleArea))
