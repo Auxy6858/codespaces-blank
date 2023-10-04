fdfc115c5336a6d6a168d161c4affbd96a29f591
Auxy6858 <71662994+Auxy6858@users.noreply.github.com>
2023-10-04 21:36:40 +0100

    Update circlething.py to loop

--- circlething.py
+++ circlething.py
@@ -1,4 +1,12 @@
 Pi = float(3.142)
-UserRadius = float(input("What's the radius of the circle?\n"))
-CircleArea = float(UserRadius*UserRadius*Pi)
-print("The area is ", str(CircleArea))
+REPEAT = True
+while REPEAT == True:
+  Userchoice = input("Do you want to run the program (y/n) ")
+  if Userchoice.lower == "y": 
+    UserRadius = float(input("What's the radius of the circle?\n"))
+    CircleArea = float(UserRadius*UserRadius*Pi)
+    print("The area is ", str(CircleArea))
+  elif Userchoice.lower == "n":
+    break
+  else:
+    print("\n")
